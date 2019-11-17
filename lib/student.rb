8b7e8ecded9d8773f31660e7382d2ada56508c05
Brian Pompey <brianthescholar@gmail.com>
2019-11-17 03:55:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,10 @@ class Student
 
   def save
     sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?,?)
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+    SQL
+    
 
   end
 
